e31a3b62900872c144c124e9ccca946c45b4469e
Xinyi Ou <ouxinyi@gmail.com>
2016-01-22 21:08:53 -0500

    Initial MVC project test
    Created default project to test with Azure

---
+++ CapstoneProject/CapstoneProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CapstoneProject.Startup))]
+namespace CapstoneProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
