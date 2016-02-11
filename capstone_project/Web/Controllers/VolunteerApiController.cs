b189145a075b52f325598c3071bfab1aecdfb6cb
Eric Maino <ericmai@microsoft.com>
2016-02-11 11:42:04 -0800

    Deleting old notes

--- capstone_project/Web/Controllers/VolunteerApiController.cs
+++ capstone_project/Web/Controllers/VolunteerApiController.cs
@@ -10,41 +10,6 @@
 
     public class VolunteerApiController : ApiController
     {
-        //IVolunteerService (get from factory...look into packages)
-        //factory in startup
-        //Take it as a parameter
-        //Configure app to get the service
 
-        // GET: api/VolunteerApi
-        //return IVolunteer (configured in Serializer)
-
-        // IVolunteerService volunteerService = new IVolunteerService();
-        
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "volunteer1", "volunteer2" };
-        }
-
-        // GET: api/VolunteerApi/5
-        public string Get(int id)
-        {
-            return "volunteer" + id;
-        }
-
-        // POST: api/VolunteerApi
-        public void Post([FromBody]string value)
-        {
-            
-        }
-
-        // PUT: api/VolunteerApi/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/VolunteerApi/5
-        public void Delete(int id)
-        {
-        }
     }
 }
