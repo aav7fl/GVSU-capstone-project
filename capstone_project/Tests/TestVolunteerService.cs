587676825598516bd6ef12e5a98baa8e6c80af86
Xinyi Ou <ouxinyi@gmail.com>
2016-02-29 03:16:20 -0500

    Updated test to work with new volunteer service contract

--- capstone_project/Tests/TestVolunteerService.cs
+++ capstone_project/Tests/TestVolunteerService.cs
@@ -4,27 +4,24 @@ namespace GVSU.Tests
     using System;
     using Contracts;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using Serialization;
+    using Serialization.DTO;
     using FluentAssertions;
 
     [TestClass]
-    public class TestVolunteerService : SerivceTestBase<IVolunteerService>
+    public class TestVolunteerService : ServiceTestBase<IVolunteerService>
     {
         [TestMethod]
         public void CreateVolunteer()
         {
             var result = this.Service.CreateVolunteer(new Volunteer()
             {
-                User = new User()
-                {
-                    ContactInfo = new ContactInfo()
-                    {
-                        Email = "dontusthis@gvsu.edu"
-                    }
+                User = new User {
+                     FirstName = "Test",
+                     LastName = "Insert"
                 }
             });
 
-            result.Should().NotBeNullOrEmpty();
+            result.GetType().ShouldBeEquivalentTo(typeof(int));
         }
 
         [TestMethod]
@@ -35,21 +32,4 @@ namespace GVSU.Tests
         }
     }
 }
-
-
-//repository = information storage
-
-//irepository = layer that abstracts data storage 
-
-//implement irepository => physically store/delete/add data
-
-
-//tests directly on the irepository (running simulator in the background)
-//tests directly on the service layer (pass your already tested simulator to create a new service)
-
-//web api is tested
-//go and build a ui
-
-//test with manual testing
-//build automated tests with selenium 
 
