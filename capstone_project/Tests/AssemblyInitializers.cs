45bce311afdae95e333a67edf326579e1915056b
Kyle Niewiada <kniewia@gmail.com>
2016-03-19 14:40:41 -0400

    add selenium test suite. Add chrome driver with Firefox and Google Chrome test cases.

--- capstone_project/Tests/AssemblyInitializers.cs
+++ capstone_project/Tests/AssemblyInitializers.cs
@@ -7,16 +7,24 @@ using GVSU.BusinessLogic;
 using GVSU.Contracts;
 using GVSU.Simulators;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace GVSU.Tests
 {
     [TestClass]
     public class AssemblyInitializers
     {
+        public static IWebDriver driverFF { get; set; }
+        public static IWebDriver driverGC { get; set; }
+
         [AssemblyInitialize]
         public static void Init(TestContext context)
         {
             Factory.Register<IVolunteerService>(() => new VolunteerServiceSimulator());
+            driverFF = new FirefoxDriver();
+            driverGC = new ChromeDriver(@".\chromedriver_win32\");
         }
     }
 }
---
+++ capstone_project/Tests/SeleniumTestWebDriver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace GVSU.Tests
+{
+    [TestClass]
+    public class SeleniumTestWebDriver
+    {
+        [TestMethod]
+        public void TestFirefoxDriver()
+        {
+            AssemblyInitializers.driverFF.Navigate().GoToUrl("https://www.google.com/");
+            AssemblyInitializers.driverFF.FindElement(By.Id("lst-ib")).SendKeys("Selenium");
+            AssemblyInitializers.driverFF.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+        }
+
+        [TestMethod]
+        public void TestChromeDriver()
+        {
+            AssemblyInitializers.driverGC.Navigate().GoToUrl("https://www.google.com/");
+            AssemblyInitializers.driverGC.FindElement(By.Id("lst-ib")).SendKeys("Selenium");
+            AssemblyInitializers.driverGC.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+        }
+    }
+}
