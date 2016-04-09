dd2191844edbef8e1bbd78851dbe1026e083a135
Kyle Niewiada <kniewia@gmail.com>
2016-04-09 18:05:15 -0400

    update git ignore to ignore test config

--- capstone_project/Tests/AssemblyInitializers.cs
+++ capstone_project/Tests/AssemblyInitializers.cs
@@ -7,6 +7,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System.Configuration;
+using System.Diagnostics;
+using System.Threading;
+using System;
+using System.IO;
 
 namespace GVSU.Tests
 {
@@ -17,19 +21,20 @@ namespace GVSU.Tests
         public static IWebDriver driverGC { get; set; }
         public static List<IWebDriver> drivers = new List<IWebDriver>();
 
-        public static string WebsiteAddress {
-
-        get
+        public static string WebsiteAddress
         {
-            string url;
+            get
+            {
+                var url = "";
 #if DEBUG
-            url = ConfigurationManager.AppSettings["LocalUrl"];
+                url = ConfigurationManager.AppSettings["LocalUrl"];
 #else
-            url = "https://gvsuciscapstone.azurewebsites.net/";
+                url = "https://gvsuciscapstone.azurewebsites.net/";
 #endif
-            return url;
+                //Console.WriteLine("URL:" + ConfigurationManager.AppSettings["LocalUrl"]);
+                return url;
+            }
         }
-    }
 
         [AssemblyInitialize]
         public static void Init(TestContext context)
@@ -37,11 +42,13 @@ namespace GVSU.Tests
             Factory.Register<IVolunteerService>(() => new VolunteerServiceSimulator());
             driverFF = new FirefoxDriver();
             driverGC = new ChromeDriver(@".\chromedriver_win32\");
+            
 
             //Add drivers to the list to be used in a for-each method for browser testing
             drivers.Add(driverGC);
             drivers.Add(driverFF);
         }
+        
 
         [AssemblyCleanup]
         public static void TearDown()
@@ -49,5 +56,68 @@ namespace GVSU.Tests
             driverFF.Quit(); //exit Firefox selenium driver at the end of the test
             driverGC.Quit(); //exit Google Chrome selenium driver at the end of the test
         }
+
+
+        private Process _process;
+        private ManualResetEventSlim _manualResetEvent;
+
+        /// <summary>
+        /// Starts the IISExpress process that hosts the test site.
+        /// </summary>
+        public void Start()
+        {
+            _manualResetEvent = new ManualResetEventSlim(false);
+
+            var thread = new Thread(StartIisExpress) { IsBackground = true };
+            thread.Start();
+
+            if (!_manualResetEvent.Wait(15000))
+                throw new Exception("Could not start IIS Express");
+
+            _manualResetEvent.Dispose();
+        }
+
+        private void StartIisExpress()
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Minimized,
+                //CreateNoWindow = !_options.ShowIisExpressWindow,
+                //Arguments = string.Format("/config:\"{0}\" /systray:true", applicationHostPath)
+            };
+
+            var programfiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+            startInfo.FileName = programfiles + "\\IIS Express\\iisexpress.exe";
+
+            try
+            {
+                _process = new Process { StartInfo = startInfo };
+
+                _process.Start();
+                _manualResetEvent.Set();
+                _process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error starting IIS Express: " + ex);
+                _process.CloseMainWindow();
+                _process.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Stops the IISExpress process that hosts the test site.
+        /// </summary>
+        public void Stop()
+        {
+            if (_process == null)
+                return;
+            _process.WaitForExit(5000);
+            if (!_process.HasExited)
+                _process.Kill();
+        }
+
+
     }
 }
