d864fc62b1578d8eae59af16e890882c2fadfab0
Xinyi Ou <ouxinyi@gmail.com>
2016-04-23 12:52:53 -0400

    Bundler Capitalization Issue Fixed

--- capstone_project/Web/App_Start/BundleConfig.cs
+++ capstone_project/Web/App_Start/BundleConfig.cs
@@ -34,8 +34,8 @@ namespace Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/font-awesome.css",
-                      "~/Content/Css/site.css",
-                      "~/Content/Css/custom.css"));
+                      "~/Content/Css/Site.css",
+                      "~/Content/Css/Custom.css"));
         }
     }
 }
